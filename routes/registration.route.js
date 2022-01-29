a2fc39f48d175b92811da499309336ab32a1078d
emergentIOT <aman24january@gmail.com>
2022-01-29 16:40:06 +1100

    Updated endpoint

--- routes/registration.route.js
+++ routes/registration.route.js
@@ -7,7 +7,8 @@ const registerController = require('../controllers/registration.controller');
 
 //User Registration APIs
 router.post('/register', asyncHandler(register));
-
+router.get('/login', asyncHandler(login));
+router.get('/user', asyncHandler(getUserByEmail));
 
 /**
  * POST
@@ -19,13 +20,20 @@ async function register(req, res) {
 
         const password = req.body.password;
         const confirmPassword = req.body.confirmPassword;
-
+        if(!req.body.name) {
+            res.json({
+                success: false,
+                message: 'Name is missing'
+            })
+            return;
+        }
         if(password == confirmPassword) {
             const user = await registerController.insert(req.body);
             res.json({
                 success: true,
                 message: user
             })
+            return;
         } else {
             res.json({
                 success: false,
@@ -34,15 +42,47 @@ async function register(req, res) {
             return;
         }
 
-        res.json({
-            success: true,
-            message: 'User Register sucessfully'
-        })
+        // res.json({
+        //     success: true,
+        //     message: 'User Register sucessfully'
+        // })
 
     } catch(err) {
         res.status(400).send(err);
     }
    
+}
+
+/**
+ * GET
+ * /login
+ * - Login a User
+ */
+async function login(req, res) {
+
+    try {
+        const { email, password } = req.body;
+        const userEmail = await registerUser.findOne({email});
+        res.json({
+            success: true,
+            message: 'Successfully login.' + userEmail
+        })
+    } catch(error) {
+        res.status(400).message(error);
+    }
+} 
+
+/**
+ * GET
+ * /user
+ * - Get a user with email
+ */
+async function getUserByEmail(req, res) {
+
+    res.json({
+        success: true,
+        message: 'Datareceived'
+    })
 }
 
 module.exports = router;
